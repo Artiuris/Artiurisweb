8d9c6955895db171ec542aa1c0783f459a763533
flashplansapp-blip <flashplansapp@gmail.com>
2026-05-02 13:08:46 +0200

    fix: exclude images from serverless bundles to fix 250MB limit

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingExcludes: {
+    "*": [
+      "./public/images/artworks/**",
+      "./public/images/**/*.jpg",
+      "./public/images/**/*.jpeg",
+      "./public/images/**/*.png",
+      "./public/images/**/*.JPG",
+      "./public/images/**/*.PNG",
+    ],
+  },
 };
 
 export default nextConfig;
